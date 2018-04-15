4be601268796267f393b55b1f8b6f76c20cb8926
leandrocdm <leandrocdm@live.com>
2018-04-15 16:16:43 -0300

    Atualizando Workspace

---
+++ HelloUniverse/HelloUniverse/HelloViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HelloViewController.swift
+//  HelloUniverse
+//
+//  Created by Usuário Convidado on 16/03/2018.
+//  Copyright © 2018 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class HelloViewController: UIViewController {
+
+    @IBOutlet weak var lblMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeLabel(_ sender: Any) {
+        lblMessage.text = "Olá Universo"
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
