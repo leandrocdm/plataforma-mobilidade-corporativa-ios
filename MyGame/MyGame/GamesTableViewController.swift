9547176eb090b0bf359bc502d9c06ce21f817f4c
leandrocdm <leandrocdm@live.com>
2018-08-31 18:12:41 -0300

    Feature/MyGame

---
+++ MyGame/MyGame/GamesTableViewController.swift
@@ -0,0 +1,135 @@
+//
+//  GamesTableViewController.swift
+//  MyGame
+//
+//  Created by Usuário Convidado on 24/08/2018.
+//  Copyright © 2018 FIAP. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class GamesTableViewController: UITableViewController {
+
+    var fetchedResultController: NSFetchedResultsController<Game>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadGames()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //Vamos de is
+//        if sender is UITableViewCell {
+//        }
+        
+        //Vamos de guard let
+        guard let _ = sender as? UITableViewCell else {return}
+        guard let vc = segue.destination as? GameViewController else {return}
+        let game = fetchedResultController.object(at: tableView.indexPathForSelectedRow!)
+        vc.game = game
+    }
+
+    func loadGames() {
+        
+        let fetchRequest: NSFetchRequest<Game> = Game.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "console", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultController.delegate = self
+        
+        try? fetchedResultController.performFetch()
+        
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return fetchedResultController.fetchedObjects?.count ?? 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        let game = fetchedResultController.object(at: indexPath)
+        cell.textLabel?.text = game.console
+        cell.detailTextLabel?.text = game.name
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let game = fetchedResultController.object(at: indexPath)
+            context.delete(game)
+            try? context.save()
+        }
+    }
+
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension GamesTableViewController: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        tableView.reloadData()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
